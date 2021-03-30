3fa121de0dc341ba2bcda4b621221b9107d17497
hummingengineer <56923582+hummingengineer@users.noreply.github.com>
2021-03-30 16:51:38 +0900

    feat: Add dataSources to ApolloServer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const AbandonedAnimalAPI = require('./datasources/abandoned-animal-api');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      abandonedAnimalAPI: new AbandonedAnimalAPI(),
+    };
+  },
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
